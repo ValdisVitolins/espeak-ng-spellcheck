a22d1859f4df03048f565492e74f2545422059bd
Valdis Vitolins <valdis.vitolins@odo.lv>
2018-04-06 14:30:39 +0300

    Rename MBROLA scripts

--- check-mbrola-speech.sh
+++ check-mbrola-speech.sh
@@ -5,5 +5,5 @@ log="${lang}-log.txt"
 rm $log 2>/dev/null
 while IFS='' read -r line || [[ -n "$line" ]]; do
     printf "$(echo "$line"|tr "\r" "\t")" >> $log
-    unbuffer espeak-ng -x -q -vmb-${lang}${no} "$line" >> $log 2>&1
+    unbuffer espeak-ng -x -q -vmb-${lang}${no} "[[$line]]" >> $log 2>&1
 done < "${lang}-words.txt"
